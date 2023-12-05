034da3cb2dd81baef4a0e3db3384f22fe8f2fde0
Mgrdich <mgotm13@gmail.com>
2023-12-05 15:22:06 +0400

    Lexer code fixes

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -5,6 +5,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"unicode"
+	"unicode/utf8"
 )
 
 type LexToken struct {
@@ -16,6 +18,18 @@ type Lexer struct {
 	directory string
 	scanner   *bufio.Scanner
 	tokens    []LexToken
+}
+
+func lower(ch rune) rune     { return ('a' - 'A') | ch } // returns lower-case ch iff ch is ASCII letter
+func isDecimal(ch rune) bool { return '0' <= ch && ch <= '9' }
+func isHex(ch rune) bool     { return '0' <= ch && ch <= '9' || 'a' <= lower(ch) && lower(ch) <= 'f' }
+
+func isLetter(ch rune) bool {
+	return 'a' <= lower(ch) && lower(ch) <= 'z' || ch == '_' || ch >= utf8.RuneSelf && unicode.IsLetter(ch)
+}
+
+func isDigit(ch rune) bool {
+	return isDecimal(ch) || ch >= utf8.RuneSelf && unicode.IsDigit(ch)
 }
 
 // Tokenize will tokenize the whole file
@@ -48,8 +62,27 @@ func (lex *Lexer) scanStart() {
 	}
 }
 
+func getRune(bytes []byte) ([]rune, error) {
+	var runes []rune
+
+	for len(bytes) > 0 {
+		r, size := utf8.DecodeRune(bytes)
+		if r == utf8.RuneError && size == 1 {
+			return nil, fmt.Errorf("something in getting rune error")
+		}
+		runes = append(runes, r)
+		bytes = bytes[size:]
+	}
+
+	return runes, nil
+}
+
 func (lex *Lexer) getNextToken() (token.Token, string) {
-	ch := lex.scanner.Bytes() // TODO maybe keep the bytes in the array for next peek functionality
+	ch, err := getRune(lex.scanner.Bytes())
+
+	if err != nil {
+		return token.ILLEGAL, ""
+	}
 
 	fmt.Println(ch, lex.scanner.Text())
 
