4737b35e2f57ea2c52469a67485d060110ee94d4
Mgrdich <mgotm13@gmail.com>
2023-12-05 17:31:34 +0400

    Lexer fixes to integrate the whole system

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -17,19 +17,15 @@ type LexToken struct {
 type Lexer struct {
 	directory string
 	scanner   *bufio.Scanner
-	tokens    []LexToken
+	Tokens    []LexToken
+	chars     []rune // current Token position
+	index     int    // current Token index
 }
 
 func lower(ch rune) rune     { return ('a' - 'A') | ch } // returns lower-case ch iff ch is ASCII letter
 func isDecimal(ch rune) bool { return '0' <= ch && ch <= '9' }
-func isHex(ch rune) bool     { return '0' <= ch && ch <= '9' || 'a' <= lower(ch) && lower(ch) <= 'f' }
-
 func isLetter(ch rune) bool {
 	return 'a' <= lower(ch) && lower(ch) <= 'z' || ch == '_' || ch >= utf8.RuneSelf && unicode.IsLetter(ch)
-}
-
-func isDigit(ch rune) bool {
-	return isDecimal(ch) || ch >= utf8.RuneSelf && unicode.IsDigit(ch)
 }
 
 // Tokenize will tokenize the whole file
@@ -55,14 +51,15 @@ func (lex *Lexer) Tokenize() {
 func (lex *Lexer) scanStart() {
 	for lex.scanner.Scan() {
 		tok, lit := lex.getNextToken()
-		lex.tokens = append(lex.tokens, LexToken{
+		lex.Tokens = append(lex.Tokens, LexToken{
 			tok: tok,
 			lit: lit,
 		})
 	}
+	fmt.Println(lex.Tokens)
 }
 
-func getRune(bytes []byte) ([]rune, error) {
+func convertByteToRune(bytes []byte) ([]rune, error) {
 	var runes []rune
 
 	for len(bytes) > 0 {
@@ -77,16 +74,102 @@ func getRune(bytes []byte) ([]rune, error) {
 	return runes, nil
 }
 
-func (lex *Lexer) getNextToken() (token.Token, string) {
-	ch, err := getRune(lex.scanner.Bytes())
+func (lex *Lexer) initializeRune(chars []rune) {
+	lex.chars = chars
+	lex.index = -1
+}
 
-	if err != nil {
-		return token.ILLEGAL, ""
+func (lex *Lexer) getRune() rune {
+	if len(lex.chars) == 0 {
+		panic("character is not defined")
 	}
 
-	fmt.Println(ch, lex.scanner.Text())
+	return lex.chars[lex.index]
+}
 
-	return token.INTEGER, ""
+func (lex *Lexer) nextCh() bool {
+	if len(lex.chars) == 0 {
+		panic("character is not defined")
+	}
+
+	if len(lex.chars) == lex.index+1 {
+		return false
+	}
+
+	lex.index = lex.index + 1
+
+	return true
+}
+
+func (lex *Lexer) peekNextCh() rune {
+	if len(lex.chars) == 0 {
+		panic("character is not defined")
+	}
+
+	if len(lex.chars) == lex.index+1 {
+		return 0
+	}
+
+	return lex.chars[lex.index+1]
+}
+
+func (lex *Lexer) scanIdentifier() token.Token {
+	return token.INTEGER
+}
+
+func (lex *Lexer) scanNumber() token.Token {
+	return token.INTEGER
+}
+
+// getNextToken this will tokenize by iterating between spaces
+func (lex *Lexer) getNextToken() (token.Token, string) {
+	chars, err := convertByteToRune(lex.scanner.Bytes())
+	tok := token.ILLEGAL
+
+	if err != nil {
+		return tok, lex.scanner.Text()
+	}
+
+	if len(chars) == 1 {
+		c := chars[0]
+		switch c {
+		case '+':
+			tok = token.ADD
+		case '-':
+			tok = token.SUB
+		case '*':
+			tok = token.MUL
+		case '/':
+			tok = token.QUO
+		case '.':
+			tok = token.DOT
+		default:
+			if isDecimal(c) {
+				tok = token.INTEGER
+			}
+		}
+
+		return tok, lex.scanner.Text()
+	}
+
+	// bigger tokens
+	lex.initializeRune(chars)
+	lex.nextCh()
+
+	switch c := lex.getRune(); {
+	case isLetter(c):
+		tok = lex.scanIdentifier()
+	case isDecimal(c) || (c == '.' && isDecimal(lex.peekNextCh())):
+		tok = lex.scanNumber()
+	case c == '.':
+		// move
+		lex.nextCh()
+		if c == lex.getRune() {
+			tok = token.PRINT
+		}
+	}
+
+	return tok, lex.scanner.Text()
 }
 
 func GetLexer(directory string) Lexer {
