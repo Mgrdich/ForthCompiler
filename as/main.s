346a15a2393a1bc5101a115ded9dd1ff17b2c790
Mgrdich <mgotm13@gmail.com>
2023-12-07 02:02:10 +0400

    test print functionality

--- as/main.s
+++ as/main.s
@@ -1,8 +1,16 @@
+.section .data
+word:
+    .asciz "Hello"
+
 .section .text
 .globl _start
 _start:
     mov $42, %rsi
     call print
+    /* mov $word, %rsi */
+    /* call printwln */
+    /* mov $42, %rsi */
+    /* call println */
 exit:
     mov $60, %rax
     xor %rdi, %rdi
