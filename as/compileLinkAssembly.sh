ba893600423fd0693022fd6bbd90806bb02aabe0
Mgrdich <mgotm13@gmail.com>
2023-12-11 01:23:52 +0400

    Finalize the compiler linking step

--- as/compileLinkAssembly.sh
+++ as/compileLinkAssembly.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-as -o main.o tmp_main.s
+as -o tmp_main.o tmp_main.s
 as -o print.o print.s
-ld -o main main.o print.o -lc -dynamic-linker /lib64/ld-linux-x86-64.so.2
-cp main ../main
+ld -o mf tmp_main.o print.o -lc -dynamic-linker /lib64/ld-linux-x86-64.so.2
+cp mf ../mf
